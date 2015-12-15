9043ba025fbe651939607a70e48f7f4040902dc7
Marybeth Snodgrass <msnodgrass25@gmail.com>
2015-12-15 14:09:12 -0600

    collapse funct working

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("capstoneApp", ["ngRoute", "firebase"]);
+var app = angular.module("capstoneApp", ["ngRoute", "firebase", "ui.bootstrap"]);
 app.config(['$routeProvider',function($routeProvider) {
     $routeProvider.
         when('/', {
