b696e43c80f86b7e8480fa9cdc707ff52d2d30d9
Marybeth Snodgrass <msnodgrass25@gmail.com>
2015-12-15 12:22:50 -0600

    search foods in fb working

--- app/controllers/additemCtrl.js
+++ app/controllers/additemCtrl.js
@@ -1,7 +1,13 @@
-app.controller("additemCtrl", ["$scope", "Auth",function($scope, Auth) {
+app.controller("additemCtrl", ["$scope", "Auth", "$firebaseArray" ,function($scope, Auth, $firebaseArray) {
     $scope.date = new Date();
+
+    $scope.searchInput = '';
 
     $scope.purchaseDate = {
          value: $scope.date
-       };
+    };
+    var ref = new Firebase("https://perishibles.firebaseio.com/foods/");
+
+    $scope.data = $firebaseArray(ref);
+
 }]);
